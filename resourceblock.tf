808cdc1c077e166c32fb1b921fb93aa8e4c5ad69
sujeeth29 <150421716+sujeeth29@users.noreply.github.com>
2024-03-12 18:51:24 +0530

    Update resourceblock.tf

--- resourceblock.tf
+++ resourceblock.tf
@@ -36,7 +36,7 @@ resource "aws_instance" "ec2" {
     instance_type = "t2.micro"
     key_name = "security"
     subnet_id = "subnet-093c04d40cd777b27"
-    security_group = aws_security_group.ec2_sg.id
+    security_groups = aws_security_group.ec2_sg.id
     tags = {
         Name = "terraform-ec2-1"
     }
